b030d44b6d1b8cd5a47eb703773daea6fe18b152
UlysC <ulys.chambard@gmail.com>
2022-05-03 16:24:48 +0200

    add param

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $user = $request->query->get('hello') ?? 'Anon';
+        $greet = sprintf('<h1>Hello %s</h1>', htmlspecialchars($user));
         return new Response(<<<EOF
             <html>
                 <body>
+                    $greet
                     <img src="/images/underconstruction.gif"/>
                 </body>
             </html>
